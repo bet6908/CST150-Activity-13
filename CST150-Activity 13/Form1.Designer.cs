3ea6eb72c357a293462178dd7221f413eabf2be5
beto6 <beto6@DESKTOP-CBL37F1>
2022-04-20 20:33:40 -0600

    Add project files.

---
+++ CST150-Activity 13/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CST150_Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_a1 = new System.Windows.Forms.Button();
+            this.btn_a2 = new System.Windows.Forms.Button();
+            this.btn_a3 = new System.Windows.Forms.Button();
+            this.btn_b1 = new System.Windows.Forms.Button();
+            this.btn_b2 = new System.Windows.Forms.Button();
+            this.btn_b3 = new System.Windows.Forms.Button();
+            this.btn_c1 = new System.Windows.Forms.Button();
+            this.btn_c2 = new System.Windows.Forms.Button();
+            this.btn_c3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_c3);
+            this.panel1.Controls.Add(this.btn_c2);
+            this.panel1.Controls.Add(this.btn_c1);
+            this.panel1.Controls.Add(this.btn_b3);
+            this.panel1.Controls.Add(this.btn_b2);
+            this.panel1.Controls.Add(this.btn_b1);
+            this.panel1.Controls.Add(this.btn_a3);
+            this.panel1.Controls.Add(this.btn_a2);
+            this.panel1.Controls.Add(this.btn_a1);
+            this.panel1.Location = new System.Drawing.Point(41, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_a1
+            // 
+            this.btn_a1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_a1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_a1.Location = new System.Drawing.Point(0, 0);
+            this.btn_a1.Name = "btn_a1";
+            this.btn_a1.Size = new System.Drawing.Size(150, 150);
+            this.btn_a1.TabIndex = 0;
+            this.btn_a1.UseVisualStyleBackColor = false;
+            this.btn_a1.Click += new System.EventHandler(this.btn_a1_Click);
+            // 
+            // btn_a2
+            // 
+            this.btn_a2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_a2.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_a2.Location = new System.Drawing.Point(150, 0);
+            this.btn_a2.Name = "btn_a2";
+            this.btn_a2.Size = new System.Drawing.Size(150, 150);
+            this.btn_a2.TabIndex = 1;
+            this.btn_a2.UseVisualStyleBackColor = false;
+            this.btn_a2.Click += new System.EventHandler(this.btn_a2_Click);
+            // 
+            // btn_a3
+            // 
+            this.btn_a3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_a3.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_a3.Location = new System.Drawing.Point(300, 0);
+            this.btn_a3.Name = "btn_a3";
+            this.btn_a3.Size = new System.Drawing.Size(150, 150);
+            this.btn_a3.TabIndex = 2;
+            this.btn_a3.UseVisualStyleBackColor = false;
+            this.btn_a3.Click += new System.EventHandler(this.btn_a3_Click);
+            // 
+            // btn_b1
+            // 
+            this.btn_b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_b1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_b1.Location = new System.Drawing.Point(0, 150);
+            this.btn_b1.Name = "btn_b1";
+            this.btn_b1.Size = new System.Drawing.Size(150, 150);
+            this.btn_b1.TabIndex = 3;
+            this.btn_b1.UseVisualStyleBackColor = false;
+            this.btn_b1.Click += new System.EventHandler(this.btn_b1_Click);
+            // 
+            // btn_b2
+            // 
+            this.btn_b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_b2.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_b2.Location = new System.Drawing.Point(150, 150);
+            this.btn_b2.Name = "btn_b2";
+            this.btn_b2.Size = new System.Drawing.Size(150, 150);
+            this.btn_b2.TabIndex = 4;
+            this.btn_b2.UseVisualStyleBackColor = false;
+            this.btn_b2.Click += new System.EventHandler(this.btn_b2_Click);
+            // 
+            // btn_b3
+            // 
+            this.btn_b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_b3.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_b3.Location = new System.Drawing.Point(300, 150);
+            this.btn_b3.Name = "btn_b3";
+            this.btn_b3.Size = new System.Drawing.Size(150, 150);
+            this.btn_b3.TabIndex = 5;
+            this.btn_b3.UseVisualStyleBackColor = false;
+            this.btn_b3.Click += new System.EventHandler(this.btn_b3_Click);
+            // 
+            // btn_c1
+            // 
+            this.btn_c1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_c1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_c1.Location = new System.Drawing.Point(0, 300);
+            this.btn_c1.Name = "btn_c1";
+            this.btn_c1.Size = new System.Drawing.Size(150, 150);
+            this.btn_c1.TabIndex = 6;
+            this.btn_c1.UseVisualStyleBackColor = false;
+            this.btn_c1.Click += new System.EventHandler(this.btn_c1_Click);
+            // 
+            // btn_c2
+            // 
+            this.btn_c2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_c2.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_c2.Location = new System.Drawing.Point(150, 300);
+            this.btn_c2.Name = "btn_c2";
+            this.btn_c2.Size = new System.Drawing.Size(150, 150);
+            this.btn_c2.TabIndex = 7;
+            this.btn_c2.UseVisualStyleBackColor = false;
+            this.btn_c2.Click += new System.EventHandler(this.btn_c2_Click);
+            // 
+            // btn_c3
+            // 
+            this.btn_c3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_c3.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_c3.Location = new System.Drawing.Point(300, 300);
+            this.btn_c3.Name = "btn_c3";
+            this.btn_c3.Size = new System.Drawing.Size(150, 150);
+            this.btn_c3.TabIndex = 8;
+            this.btn_c3.UseVisualStyleBackColor = false;
+            this.btn_c3.Click += new System.EventHandler(this.btn_c3_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(222, 542);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "New Game";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(533, 596);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_c3;
+        private System.Windows.Forms.Button btn_c2;
+        private System.Windows.Forms.Button btn_c1;
+        private System.Windows.Forms.Button btn_b3;
+        private System.Windows.Forms.Button btn_b2;
+        private System.Windows.Forms.Button btn_b1;
+        private System.Windows.Forms.Button btn_a3;
+        private System.Windows.Forms.Button btn_a2;
+        private System.Windows.Forms.Button btn_a1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ CST150-Activity 13/Form1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST150_Activity_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Starting the game with X 
+        bool isOTurn = true;
+        String winner = "";
+        int turn_tracker = 0;
+
+
+        // Creating 2D array to keep track of Tic-Tac-Toe Board 
+        const int ROWS = 3;
+        const int COLS = 3;
+        string[,] tttBoard = new string[ROWS, COLS];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+                btn_a1.Text = "";
+                btn_a2.Text = "";
+                btn_a3.Text = "";
+                btn_b1.Text = "";
+                btn_b2.Text = "";
+                btn_b3.Text = "";
+                btn_c1.Text = "";
+                btn_c2.Text = "";
+                btn_c3.Text = "";
+
+            Array.Clear(tttBoard, 0, tttBoard.Length);
+            
+        }
+        private void button_click(object sender, EventArgs e)
+        {
+            Button s = (Button)sender;
+            if (isOTurn)
+                s.Text = "X";
+                
+            else
+                s.Text = "Y";
+
+            isOTurn = !isOTurn;
+            s.Enabled = false;
+
+            gameWinner();
+
+        }
+
+        bool gameWinner()
+        {
+
+
+
+
+            // if top row is the same
+            if (tttBoard[0, 0] == "X" && tttBoard[0, 1] == "X" && tttBoard[0, 2] == "X")
+            {
+                winner = "X";
+                return true;
+
+            }
+
+            if (tttBoard[0, 0] == "O" && tttBoard[0, 1] == "O" && tttBoard[0, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+            // if middle row is the same
+            if (tttBoard[1, 0] == "X" && tttBoard[1, 1] == "X" && tttBoard[1, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (tttBoard[1, 0] == "O" && tttBoard[1, 1] == "O" && tttBoard[1, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+            // if bottom row is the same
+            if (tttBoard[2, 0] == "X" && tttBoard[2, 1] == "X" && tttBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (tttBoard[2, 0] == "O" && tttBoard[2, 1] == "O" && tttBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+            // if left coloumn is the same 
+            if (tttBoard[0, 0] == "X" && tttBoard[1, 0] == "X" && tttBoard[2, 0] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (tttBoard[0, 0] == "O" && tttBoard[1, 0] == "O" && tttBoard[2, 0] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            // if middle coloumn is the same 
+            if (tttBoard[0, 1] == "X" && tttBoard[1, 1] == "X" && tttBoard[2, 1] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (tttBoard[0, 1] == "O" && tttBoard[1, 1] == "O" && tttBoard[2, 1] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            // if right column is the same 
+            if (tttBoard[0, 2] == "X" && tttBoard[1, 2] == "X" && tttBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+            if (tttBoard[0, 2] == "O" && tttBoard[1, 2] == "O" && tttBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            // if diagonally the same 
+
+            if (tttBoard[0, 0] == "X" && tttBoard[1, 1] == "X" && tttBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (tttBoard[0, 0] == "O" && tttBoard[1, 1] == "O" && tttBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            if (tttBoard[0, 2] == "X" && tttBoard[1, 1] == "X" && tttBoard[2, 0] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+            if (tttBoard[0, 2] == "O" && tttBoard[1, 1] == "O" && tttBoard[2, 0] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+
+
+            return false;
+
+
+
+        }
+
+        private void btn_a1_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_a1.Text = "X";
+                tttBoard[0, 0] = "X";
+                isOTurn = false;
+
+
+            }
+            else
+            {
+                btn_a1.Text = "O";
+                tttBoard[0, 0] = "O";
+                isOTurn = true;
+
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_a2_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_a2.Text = "X";
+                tttBoard[0, 1] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_a2.Text = "O";
+                tttBoard[0, 1] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_a3_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_a3.Text = "X";
+                tttBoard[0, 2] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_a3.Text = "O";
+                tttBoard[0, 2] = "O";
+                isOTurn = true;
+            }
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_b1_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_b1.Text = "X";
+                tttBoard[1, 0] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_b1.Text = "O";
+                tttBoard[1, 0] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_b2_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_b2.Text = "X";
+                tttBoard[1, 1] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_b2.Text = "O";
+                tttBoard[1, 1] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_b3_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_b3.Text = "X";
+                tttBoard[0, 2] = "X";
+                isOTurn = false;
+
+            }
+            else
+            {
+                btn_b3.Text = "O";
+                tttBoard[0, 2] = "O";
+                isOTurn = true;
+            }
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_c1_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_c1.Text = "X";
+                tttBoard[2, 0] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_c1.Text = "O";
+                tttBoard[2, 0] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btn_c2_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_c2.Text = "X";
+                tttBoard[2, 1] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_c2.Text = "O";
+                tttBoard[2, 1] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void btn_c3_Click(object sender, EventArgs e)
+        {
+            if (isOTurn)
+            {
+                btn_c3.Text = "X";
+                tttBoard[2, 2] = "X";
+                isOTurn = false;
+            }
+            else
+            {
+                btn_c3.Text = "O";
+                tttBoard[2, 2] = "O";
+                isOTurn = true;
+            }
+
+            bool winner_winner = gameWinner();
+            if (winner_winner)
+            {
+                MessageBox.Show(winner + " Player WON!!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
